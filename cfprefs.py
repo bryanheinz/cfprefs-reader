085b365f911c0fc826010e6061f42ab95d96b3f4
Bryan Heinz <bryanh@Bryan-Heinzs-MacBookPro.local>
2017-10-19 17:21:25 -0500

    changed to allow argv input, added help line.

--- cfprefs.py
+++ cfprefs.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python
 
-# Copyright 2016 dataJAR Ltd.
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Script forked and tweaked from https://github.com/dataJAR/jnuc2016
 
 import CoreFoundation
+from sys import argv
 
-domain = 'com.apple.appstore'
-key = 'restrict-store-require-admin-to-install'
+if '-h' in argv or len(argv) == 1:
+    print("")
+    print("python prefs.py <com.domain.name> <key>")
+    print("")
+    exit(0)
+
+script, domain, key = argv
 
 key_value = CoreFoundation.CFPreferencesCopyAppValue(key, domain)
 print 'Key Value = ', key_value
